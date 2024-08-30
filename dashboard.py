8ae4446a30475005a584a250f77ae3a115d5e781
Desmond Onam <desmondonam@gmail.com>
2024-08-30 18:07:17 +0300

    modified the dash

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,6 @@
 # Import the neccesary modules
 import streamlit as st
 import pandas as pd
-# from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 
