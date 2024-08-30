500b626045433f5d2fba373bad82672c21e42bef
Desmond Onam <desmondonam@gmail.com>
2024-08-30 18:00:51 +0300

    corrected the codes

--- dashboard.py
+++ dashboard.py
@@ -1,30 +1,32 @@
 # Import the neccesary modules
 import streamlit as st
 import pandas as pd
-from sklearn.datasets import load_iris
+# from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 
 
 # Load and prepare
-iris = load_iris()
-df = pd.DataFrame(data = iris.data, columns= iris.feature_names)
-df["Species"] = iris.target
-df['Species'] = df['Species'].map({0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'})
+# iris = load_iris()
+path = "../Project_/Iris.csv"
+df = pd.read_csv(path)
+# df = pd.DataFrame(data = iris.data, columns= iris.feature_names)
+# df["Species"] = iris.target
+# df['Species'] = df['Species'].map({0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'})
 
 # Sidebar from user input
 st.sidebar.header("Input Features")
 
 def user_input_features():
-    sepal_length = st.sidebar.slider('sepal length (cm)', float(df['sepal length (cm)'].min()), float(df['sepal length (cm)'].max()), float(df['sepal length (cm)'].mean()))
-    sepal_width = st.sidebar.slider('sepal width (cm)', float(df['sepal width (cm)'].min()), float(df['sepal width (cm)'].max()), float(df['sepal width (cm)'].mean()))
-    petal_length = st.sidebar.slider('petal length (cm)', float(df['petal length (cm)'].min()), float(df['petal length (cm)'].max()), float(df['petal length (cm)'].mean()))
-    petal_width = st.sidebar.slider('petal width (cm)', float(df['petal width (cm)'].min()), float(df['petal width (cm)'].max()), float(df['petal width (cm)'].mean()))
+    sepal_length = st.sidebar.slider('SepalLengthCm', float(df['SepalLengthCm'].min()), float(df['SepalLengthCm'].max()), float(df['SepalLengthCm'].mean()))
+    sepal_width = st.sidebar.slider('SepalWidthCm', float(df['SepalWidthCm'].min()), float(df['SepalWidthCm'].max()), float(df['SepalWidthCm'].mean()))
+    petal_length = st.sidebar.slider('PetalLengthCm', float(df['PetalLengthCm'].min()), float(df['PetalLengthCm'].max()), float(df['PetalLengthCm'].mean()))
+    petal_width = st.sidebar.slider('PetalWidthCm', float(df['PetalWidthCm'].min()), float(df['PetalWidthCm'].max()), float(df['PetalWidthCm'].mean()))
 
-    data = {'sepal length (cm)' : sepal_length,
-            'sepal width (cm)' : sepal_width,
-            'petal length (cm)': petal_length,
-            'petal width (cm)': petal_width}
+    data = {'SepalLengthCm' : sepal_length,
+            'SepalWidthCm' : sepal_width,
+            'PetalLengthCm': petal_length,
+            'PetalWidthCm': petal_width}
     features = pd.DataFrame(data, index = [0])
     return features
 
